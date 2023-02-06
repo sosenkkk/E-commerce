4d541eeeecb05560859f5434e49c092d93f8fd68
Shashank Raj <shashankraj3636@gmail.com>
2023-02-07 03:43:38 +0530

    error hai

--- routes.js
+++ routes.js
@@ -12,6 +12,7 @@ const requestHandler = (req, res) => {
     res.write('</html>');
     return res.end();
   }
+  
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
@@ -28,11 +29,10 @@ const requestHandler = (req, res) => {
       });
     });
   }
-
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from lol my Node.js Server!</h1></body>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
 };
