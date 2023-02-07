9961eb4bb75eed29f01d8fd695855bee3b3eee89
Shashank Raj <shashankraj3636@gmail.com>
2023-02-08 03:36:14 +0530

    3rd

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const http = require('http');
+const bodyParser= require('body-parser');
 
 const express= require('express');
 
-// const routes = require('./routes');
+const path = require('path');
 
 const app= express();
 
-const server = http.createServer(app);
+const adminRoutes= require('./routes/admin');
 
-server.listen(3000);
+const shopRoutes= require('./routes/shop');
+
+const rootDir= require('./util/path')
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(shopRoutes);
+
+app.use("/admin" ,adminRoutes);
+
+app.use((req,res, next)=>{
+    res.status(404).sendFile(path.join(rootDir, 'views', 'error.html'))
+})
+
+
+
+app.listen(3000);
