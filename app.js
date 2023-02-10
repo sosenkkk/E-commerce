6dba6a73a121fa00bac737d3bac4a77ca63db75d
Shashank Raj <shashankraj3636@gmail.com>
2023-02-11 00:19:26 +0530

    pug

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const bodyParser= require('body-parser');
-
 const express= require('express');
-
 const path = require('path');
 
 const app= express();
-
+app.set('view engine', 'pug');
+app.set('views' ,'views')
 const adminRoutes= require('./routes/admin');
-
 const shopRoutes= require('./routes/shop');
-
 const rootDir= require('./util/path')
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,10 +15,10 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(shopRoutes);
 
-app.use("/admin" ,adminRoutes);
+app.use("/admin" ,adminRoutes.routes);
 
 app.use((req,res, next)=>{
-    res.status(404).sendFile(path.join(rootDir, 'views', 'error.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,13 @@ const express= require('express');
 
 const rootDir= require('../util/path')
 
+const adminData= require('./admin');
+
 const router= express.Router();
 
 
 router.get('/',(req, res, next)=>{
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
