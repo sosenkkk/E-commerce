2167738757303a4384f3891fd68e48e2e54fbc85
Shashank Raj <shashankraj3636@gmail.com>
2023-05-13 02:07:06 +0530

    trynna deploy

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
+const dotenv = require("dotenv").config();
 const path = require("path");
-
 const express = require("express");
+const https = require("https")
 const bodyParser = require("body-parser");
-const csrf= require('csurf');
-const flash= require('connect-flash')
+const csrf = require("csurf");
+const fs = require("fs");
+const flash = require("connect-flash");
 const errorController = require("./controllers/error");
-const multer= require('multer');
-
+const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 const app = express();
-// const mongoConnect = require("./util/database").mongoConnect;
+
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
+const mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rfcsb3n.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
 const store = new MongoDBStore({
-  uri: "mongodb+srv://sosenkkk:sosenk@cluster0.rfcsb3n.mongodb.net/shop",
+  uri: mongoUrl,
   collection: "sessions",
 });
-const csrfProtection= csrf();
 
-const fileStorage= multer.diskStorage({
-  destination: (req, file, cb)=>{
-    cb(null, 'images');
+const csrfProtection = csrf();
+const privateKey = fs.readFileSync('server.key');
+const certificate= fs.readFileSync('server.cert');
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
   },
-  filename:(req, file, cb)=>{
-    cb(null,  new Date().getTime() + '-' + file.originalname);
-  }
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
 });
-const fileFilter=(req, file, cb)=>{
-  if(file.mimetype==='image/png' || file.mimetype === 'image/jpg' || file.mimetype=== 'image/jpeg'){
-    cb(null, true );
-  }else{
-    cb(null, false)
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
   }
-}
+};
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -44,10 +57,21 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", {stream: accessLogStream}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage, fileFilter:fileFilter}).single('image'));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/images',express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
   session({
@@ -62,48 +86,44 @@ app.use(csrfProtection);
 
 app.use(flash());
 
-app.use((req, res, next)=>{
-  if(!req.session.user){
+app.use((req, res, next) => {
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then((user) => {
-      if(!user){
+      if (!user) {
         return next();
       }
-      req.user= user;
-      next()
+      req.user = user;
+      next();
     })
     .catch((err) => {
       throw new Error(err);
     });
 });
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
-})
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
-app.use((error, req, res, next)=>{
-  res.redirect('/500')
-
-})
+app.use((error, req, res, next) => {
+  res.redirect("/500");
+});
 
 mongoose
-  .connect(
-    "mongodb+srv://sosenkkk:sosenk@cluster0.rfcsb3n.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(mongoUrl)
   .then((result) => {
-    
-
-    app.listen(3000);
+    // https.createServer( {key: privateKey, cert: certificate} ,app).listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
